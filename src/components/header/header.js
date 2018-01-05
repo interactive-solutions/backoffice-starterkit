3beb0930aa7d2fb89cc29d033975a293ea1c66e7
Simon Borgström <simon.borgstrom@interactivesolutions.se>
2018-01-05 17:29:06 +0100

    fixes to sticky menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,21 +8,50 @@ export class DashHeader extends Component {
     callback: PropTypes.func
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scrollOnTop: true
+    };
+  }
+
+  handleScroll = (e) => {
+    if (window.pageYOffset > 0) {
+      this.setState({ scrollOnTop: false });
+    } else {
+      this.setState({ scrollOnTop: true });
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
   render() {
+    const { scrollOnTop } = this.state;
+    let adjustMenuIcon = scrollOnTop ? '' : ' fixed-to-side';
+
     return (
       <Menu>
-        <Menu.Item position='left'>
-          <Button
-            primary
-            basic
-            icon
-            size='big'
-            onClick={this.props.callback}
-          >
-            <Icon name='sidebar'/>
-          </Button>
+        <Menu.Item position='left' className='borderless'>
+          <div className={adjustMenuIcon}>
+            <Button
+              primary
+              basic
+              icon
+              size='big'
+              onClick={this.props.callback}
+            >
+              <Icon name='sidebar' inverted={!scrollOnTop}/>
+            </Button>
+          </div>
         </Menu.Item>
-        <Menu.Item position='right' className='logo'>
+        <Menu.Item position='right' className='logo borderless'>
           <Header as='h2' textAlign='center' style={{ backgroundColor: 'transparent' }}>
             <Image src='assets/images/logo.png' size='mini'/>
             {this.props.title}
