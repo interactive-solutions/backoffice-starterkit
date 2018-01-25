2aed01616bb4a04319db4dc5dcef051d3ee5ac23
fredrik-hjarner <frederick.hjarner@gmail.com>
2018-01-25 11:44:20 +0100

    some adjustments

--- src/components/right-sidebar/right-sidebar.js
+++ src/components/right-sidebar/right-sidebar.js
@@ -5,52 +5,60 @@ import {
   Menu,
   Sidebar,
   Segment,
-  Button
+  Button,
+  Tab
 } from 'semantic-ui-react';
 
 export class RightSidebar extends React.Component {
   render() {
     const { visible } = this.props;
-    return (
-      <div>
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar
-            as={Menu}
-            animation='overlay'
-            direction='right'
-            visible={visible}
-            icon='labeled'
-            vertical
-            className='right-sidebar'
-          >
-            <Button.Group attached='top' widths={2}>
-              <Button>One</Button>
-              <Button icon><Icon name='setting'/></Button>
-            </Button.Group>
-            <Segment attached>
-              <Menu.Item name='settings'>
-                <Segment basic compact size='huge' className='basic-compact-segment'>
-                  <Icon name='setting'/>
-                  Settings
-                </Segment>
-                <Segment basic compact className='basic-compact-segment'>
-                  Manage your account
-                </Segment>
-              </Menu.Item>
 
-              <Menu.Item name='change-password'>
-                <Segment basic compact size='huge' className='basic-compact-segment'>
-                  Change password
-                  <Icon name='plus'/>
-                </Segment>
-              </Menu.Item>
-            </Segment>
-          </Sidebar>
-          <Sidebar.Pusher>
-            {this.props.children}
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </div>
+    const panes = [
+      {
+        menuItem: <Button key='tab1' className='tab-selector'>Notifications</Button>,
+        render: () => <Tab.Pane className='tab-pane'>Tab 1 Content</Tab.Pane>
+      },
+      {
+        menuItem: <Button key='tab2' icon='setting' className='tab-selector'/>,
+        render: () => (
+          <Tab.Pane className='tab-pane'>
+            <Menu.Item name='settings' className='sidebar-item'>
+              <Segment basic compact size='massive' className='basic-compact-segment'>
+                <Icon name='setting'/>
+                Settings
+              </Segment>
+              <Segment basic compact className='basic-compact-segment'>
+                Manage your account
+              </Segment>
+            </Menu.Item>
+
+            <Menu.Item name='change-password' className='sidebar-item'>
+              <Segment basic compact size='huge' className='basic-compact-segment'>
+                Change password <Icon name='plus'/>
+              </Segment>
+            </Menu.Item>
+          </Tab.Pane>
+        )
+      }
+    ];
+
+    return (
+      <Sidebar.Pushable as={Segment} style={{ marginTop: '0' }}>
+        <Sidebar
+          as={Menu}
+          animation='overlay'
+          direction='right'
+          visible={visible}
+          icon='labeled'
+          vertical
+          className='right-sidebar'
+        >
+          <Tab panes={panes} className='sidebar-tab'/>
+        </Sidebar>
+        <Sidebar.Pusher>
+          {this.props.children}
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
     );
   }
 }
